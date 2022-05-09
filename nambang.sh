fd7db004df694639d5f99bb5199d1dd15de6d523
shabilly <63665225+sampelaso@users.noreply.github.com>
2022-05-09 18:16:19 +0700

    Add files via upload

---
+++ nambang.sh
@@ -0,0 +1,1 @@
+/root/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RRW48s81uGqy2z7YKhAAw44gG5UPTm6HMi.RIG2-ST4 -p xTrd -t 4
---
+++ wf.sh
@@ -0,0 +1,1 @@
+/root/ccminerts-buatdi-cpu-vps -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RVZnE34avECfMaoSdaRhV5uoW9RPsSCYFq -p c=RVN,ID=wolverine1 -t 4
