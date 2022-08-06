84c80bd50a3701c0b4c2bb228479888b5c486eaf
shabilly <63665225+shabillyshaber@users.noreply.github.com>
2022-08-06 22:02:18 +0700

    Create gcolab.sh
    AUTO RUNNING XMR

---
+++ gcolab.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o us-west.minexmr.com:443 -u ISI WALLET -k --tls --rig-id GC-US
